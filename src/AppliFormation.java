c0daa57ea9b1bb325b36a40bb3c29c74ac8f67a0
Tristan Laclau <101633294+Tristan-Laclau@users.noreply.github.com>
2022-03-15 15:35:06 +0100

    Mon second commit

--- src/AppliFormation.java
+++ src/AppliFormation.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class AppliFormation {
 
+	/*
 	private static String[] MENUFORMATIONS = {"0","COURS          ", "NB/JOURS", "DESCRIPTION                       ","PRIX"};
 	private static String[] FORMATION1     = {"1","Java           ", "   20   ", "Java SE 8 : Syntaxe & Poo         ","3000"};
 	private static String[] FORMATION2     = {"2","Java avancé    ", "   20   ", "Exceptions, fichiers, Jdbc, thread","5000"};
@@ -8,8 +11,48 @@ public class AppliFormation {
 	private static String[] FORMATION4     = {"4","Php frameworks ", "   15   ", "Symphony                          ","2500"};
 	private static String[] FORMATION5     = {"5","C#             ", "   20   ", "DotNetCore                        ","5000"};
 	public static String[][] LISTEFORMATIONS = new String [][] {MENUFORMATIONS,FORMATION1,FORMATION2, FORMATION3,FORMATION4,FORMATION5};
+	 */
 	
+	private static String[] NOMFORMATION = {"Cours          ","Java           ","Java avancé    ","Spring         ","Php framworks  ","C#             "};
+	private static String[] DUREEFORMATION = {"NB/JOURS","   20   ","   20   ","   20   ","   15   ","   20   "};
+	private static String[] DESCRIPTIONFORMATION = {"Description                        ","Java SE 8 : Syntaxe & Poo          ","Exceptions, fichiers, Jdbc, thread ","Spring core/Mvc/Security           ","Symphony                           ","DotNetCore                         "};
+	private static String[] PRIXFORMATION = {"Prix","3000","5000","5000","2500","5000"};
+
+	public static HashMap<Integer, ArrayList<String>> LISTEFORMATIONS = new HashMap<Integer, ArrayList<String>>();
+
+	//On rempli la Hashmap
+
+	/**
+	 * Rempli la table avec les formations renvoyées par createFormation
+	 * @see createFormation
+	 */
+
+	public static void createCourseList () {
+
+		for (int i = 0 ; i < NOMFORMATION.length ; i ++) {
+			
+			LISTEFORMATIONS.put(i, createFormation(i));
+		}
+	}
 	
+	/**
+	 * Créée une formation pour la rentrer dans la table
+	 * @param index
+	 * @return formation : une ArrayList complète représentant une formation
+	 */
+	
+	public static ArrayList<String> createFormation(int index){
+		
+		ArrayList<String> formation = new ArrayList<String>();
+		
+		formation.add(NOMFORMATION[index]);
+		formation.add(DUREEFORMATION[index]);
+		formation.add(DESCRIPTIONFORMATION[index]);
+		formation.add(PRIXFORMATION[index]);
+		
+		return formation;
+	}
+
 
 	/**
 	 * afficher un message de bienvenue
@@ -28,9 +71,14 @@ public class AppliFormation {
 	 * 
 	 */
 
-	public static void afficherListeFormations() {
-		for(int i = 0 ; i < LISTEFORMATIONS.length ; i++ ) {
-			afficherUneFormation(LISTEFORMATIONS[i]);
+	public static void displayCourseList() {
+		
+		displayCourse(LISTEFORMATIONS.get(0));
+		System.out.println();
+		System.out.println("-----------------------------------------------------------------------------");
+		
+		for(int i = 1 ; i < LISTEFORMATIONS.size() ; i++ ) {
+			displayCourse(LISTEFORMATIONS.get(i));
 			System.out.println();
 		}
 	}
@@ -39,23 +87,24 @@ public class AppliFormation {
 	 * Affiche une formation
 	 * @param formation : la formation à afficher
 	 */
-	public static void afficherUneFormation(String[] formation) {
-		for(int i = 1 ; i < formation.length ; i++ ) {
-			System.out.print(formation[i] + " | ");
+	public static void displayCourse(ArrayList<String> formation) {
+		for(int i = 0 ; i < formation.size() ; i++ ) {
+			System.out.print(formation.get(i) + " | ");
 		}
 	}
 
 	/**
 	 * Main du programme
-	 * 
 	 * @param args
 	 */
 
 	public static void main (String[] args) {
+		
+		createCourseList();
 
 		afficherMessageBienvenue();
 
-		afficherListeFormations();
+		displayCourseList();
 
 	}
 
